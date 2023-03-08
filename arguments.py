4a1f410b5e62e40c804be5b8a3f56d914a5d0c48
u7245160 <u7245160@anu.edu.au>
2023-03-08 21:25:49 +1100

    Set up the API. Now has the ability to generate the Sewage / CCTV Report summary.

--- arguments.py
+++ arguments.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--openai-key", default="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")
+parser.add_argument("--openai-key", default="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")
 parser.add_argument("--model-engine", default="text-davinci-003")
 parser.add_argument("--pdf-path", default="/Users/tim/Downloads/report_examples/")
 args = parser.parse_args()
